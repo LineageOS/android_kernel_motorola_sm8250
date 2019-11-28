9d835600f4b5419086f492577d9684962f33b54d
Ravi Vembu <raviv@motorola.com>
2019-11-28 16:50:54 -0600

    asoc: msm-pcm-routing: Fixup usage of SEN_MI2S route names
    The Senary MI2S route naming is inconsistent, specifically for
    the TX paths. Use a consistent naming convention of "SEN_MI2S_TX"
    across the code. Also update the backend names to "SEN_MI2S".
    
    Change-Id: I7c7937093f64e32c02db317b8cbecf1ac7fc1966
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Mot-CRs-Fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/1463470
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- techpack/audio/asoc/msm-dai-q6-v2.c
+++ techpack/audio/asoc/msm-dai-q6-v2.c
@@ -5993,7 +5993,7 @@ static struct snd_soc_dai_driver msm_dai_q6_mi2s_dai[] = {
 		},
 		.capture = {
 			.stream_name = "Senary MI2S Capture",
-			.aif_name = "SENARY_MI2S_TX",
+			.aif_name = "SEN_MI2S_TX",
 			.rates = SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_8000 |
 			SNDRV_PCM_RATE_16000,
 			.formats = SNDRV_PCM_FMTBIT_S16_LE,
