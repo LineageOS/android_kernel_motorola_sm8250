5a04b946f9633f244374b11f1412a8a3e2d353b1
liaohj <liaohj@lenovo.com>
2020-01-07 11:55:47 +0800

    audio-kernel:nairobi evb bringup kernel driver
    - add awinic 82258 kernel4.19 driver.
    - add daillinke at machine driver
    - add litonairo config for nairobi project.
    - add awinic afe code.
    
    Change-Id: Ic5a640e7cd112d17de69c4a490d28f8281b4e1af
    Reviewed-on: https://gerrit.mot.com/1480043
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from 67c2090ca1de6bdf626db6f38802b2af5acedbe0)

---
+++ techpack/audio/config/litonairo.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#undef CONFIG_SND_SOC_CS35L41
+#undef CONFIG_CIRRUS_PLAYBACK
+#define CONFIG_SND_SOC_AWINIC_AW882XX 1
