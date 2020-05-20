769ebcfdb37ac4a3be492df9c64b723025adb6f1
dudd1 <dudd1@motorola.com>
2020-05-20 17:36:44 +0800

    ASoC:enable aw882xx vi feedback
    Add route for PRI_MI2S_RX_VI_FB_MUX, since kiev
    use primary i2s to aw882xx.
    
    Change-Id: Id05443f8774a7d97b7bc044bd2e17935a6d926ba
    Reviewed-on: https://gerrit.mot.com/1567868
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Jira Key

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -24247,7 +24247,7 @@ static const char * const wsa_rx_0_vi_fb_tx_rch_mux_text[] = {
 };
 
 static const char * const mi2s_rx_vi_fb_tx_mux_text[] = {
-	"ZERO", "SENARY_TX"
+	"ZERO", "PRI_MI2S_TX", "SENARY_TX"
 };
 
 static const char * const tert_mi2s_rx_vi_fb_tx_mux_text[] = {
@@ -24291,7 +24291,7 @@ static const int const sec_mi2s_rx_vi_fb_tx_value[] = {
 };
 
 static const int mi2s_rx_vi_fb_tx_value[] = {
-	MSM_BACKEND_DAI_MAX, MSM_BACKEND_DAI_SENARY_MI2S_TX
+	MSM_BACKEND_DAI_MAX, MSM_BACKEND_DAI_PRI_MI2S_TX, MSM_BACKEND_DAI_SENARY_MI2S_TX
 };
 
 static const int int4_mi2s_rx_vi_fb_tx_mono_ch_value[] = {
@@ -30700,6 +30700,7 @@ static const struct snd_soc_dapm_route intercon_mi2s[] = {
 	{"SEC_MI2S_TX", NULL, "BE_IN"},
 	{"SEN_MI2S_TX", NULL, "BE_IN"},
 
+	{"PRI_MI2S_RX_VI_FB_MUX", "PRI_MI2S_TX", "PRI_MI2S_TX"},
 	{"PRI_MI2S_RX_VI_FB_MUX", "SENARY_TX", "SENARY_TX"},
 	{"SEC_MI2S_RX_VI_FB_MUX", "SEC_MI2S_TX", "SEC_MI2S_TX"},
 	{"TERT_MI2S_RX_VI_FB_MUX", "TERT_MI2S_TX", "TERT_MI2S_TX"},
