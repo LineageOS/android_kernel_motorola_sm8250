a6b1b0a9dc3ea8727f4173d4740614d8ff4d3dcf
Yoon (Seungyoon) Lee <w22905@motorola.com>
2020-05-28 14:48:18 -0500

    asoc: pcm: Add SEN_TDM_RX_0 to EC Ref port
    Add SEN_TDM_RX_0 to internal EC ref port for
    AUDIO_REF_EC_UL1 MUX to use that for echo-reference.
    
    MOT-CRs-Fixed: (CR)
    Change-Id: I7b038ccae290abf2fc1671c3cf1f41dd95ec3ae9
    Signed-off-by: Yoon (Seungyoon) Lee <w22905@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1585963
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Reviewed-by: Joseph Dwyer <josephdwyer@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1669570
    Reviewed-by: Seungyoon Lee <yoon@motorola.com>

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -5570,6 +5570,10 @@ static int get_ec_ref_port_id(int value, int *index)
 		*index = 40;
 		port_id = AFE_PORT_ID_QUINARY_TDM_TX;
 		break;
+	case 41:
+	        *index = 41;
+		port_id = AFE_PORT_ID_SENARY_TDM_RX;
+		break;
 	default:
 		*index = 0; /* NONE */
 		pr_err("%s: Invalid value %d\n", __func__, value);
@@ -5628,6 +5632,7 @@ static const char *const ec_ref_rx[] = { "None", "SLIM_RX", "I2S_RX",
 	"SLIM_7_RX", "RX_CDC_DMA_RX_0", "RX_CDC_DMA_RX_1", "RX_CDC_DMA_RX_2",
 	"RX_CDC_DMA_RX_3", "TX_CDC_DMA_TX_0", "TERT_TDM_RX_2", "SEC_TDM_TX_0",
 	"DISPLAY_PORT1", "SEN_MI2S_RX", "SENARY_MI2S_TX", "QUIN_TDM_TX_0",
+	"SEN_TDM_RX_0",
 };
 
 static const struct soc_enum msm_route_ec_ref_rx_enum[] = {
@@ -29047,6 +29052,7 @@ static const struct snd_soc_dapm_route intercon_tdm[] = {
 	{"QUIN_TDM_RX_2_Voice Mixer", "VoiceMMode1", "VOICEMMODE1_DL"},
 	{"QUIN_TDM_RX_2", NULL, "QUIN_TDM_RX_2_Voice Mixer"},
 
+	{"AUDIO_REF_EC_UL1 MUX", "SEN_TDM_RX_0", "SEN_TDM_RX_0"},
 	{"VoiceMMode1_Tx Mixer", "QUAT_TDM_TX_0_MMode1", "QUAT_TDM_TX_0"},
 	{"VoiceMMode1_Tx Mixer", "SEN_MI2S_TX_MMode1", "SEN_MI2S_TX"},
 	{"VoiceMMode1_Tx Mixer", "SEN_TDM_TX_0_MMode1", "SEN_TDM_TX_0"},
