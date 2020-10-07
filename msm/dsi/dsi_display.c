4910b48055b51a54381b86d32b6aa002147ca454
shuoyan <shuoyan@motorola.com>
2020-10-07 11:13:47 -0500

    msm/dsi: add backlight debug log when panel is off
    There have few cases which no brightnes when panel on.
    Add more backlight logs to debug this issue.
    
    Propagated from I5d66c1a45affcf65ae8d800eadcfa323911054fa
    
    Change-Id: Ia3ab29e3f29a759634e7d7c02ab83bf5292a1b76
    Signed-off-by: shuoyan <shuoyan@motorola.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: maheshmk <maheshmk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1775107

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -214,6 +214,7 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 
 	mutex_lock(&panel->panel_lock);
 	if (!dsi_panel_initialized(panel)) {
+		DSI_INFO("Ignor bl_level %u as panel is not init.\n",(u32)bl_lvl);
 		rc = -EINVAL;
 		goto error;
 	}
