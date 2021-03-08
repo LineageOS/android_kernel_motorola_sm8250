90e89c80fe0b05f6d68fde4f61c4511641d806de
Erfan Abdi <erfangplus@gmail.com>
2021-03-08 14:59:33 +0330

    techpack: audio: asoc: pcm: Add SEN_TDM_RX_0 to EC_UL10
    Change-Id: I5454b60fcbe5452bbd15a98dedc1eae44af7deaf

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -29874,6 +29874,7 @@ static const struct snd_soc_dapm_route intercon_tdm[] = {
 	{"AUDIO_REF_EC_UL10 MUX", "TERT_TDM_TX_0", "TERT_TDM_TX_0"},
 	{"AUDIO_REF_EC_UL10 MUX", "TERT_TDM_RX_2", "TERT_TDM_RX_2"},
 	{"AUDIO_REF_EC_UL10 MUX", "SEC_TDM_TX_0", "SEC_TDM_TX_0"},
+	{"AUDIO_REF_EC_UL10 MUX", "SEN_TDM_RX_0", "SEN_TDM_RX_0"},
 
 	{"LSM1 Mixer", "QUIN_TDM_TX_0", "QUIN_TDM_TX_0"},
 	{"LSM1 Mixer", "TERT_TDM_TX_0", "TERT_TDM_TX_0"},
