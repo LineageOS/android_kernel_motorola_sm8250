668c1ec600c59fc44e1d792e3b8cfad3e7e9ff11
dianlujitao <dianlujitao@lineageos.org>
2022-06-07 16:04:48 +0800

    Revert "usb gadget add txfifo hint for webcam"
    This reverts commit c0dfe66247cf30dc206bcfbbc5b0adeca6c83a50.
    
    Change-Id: I6ccc00a3fe17e8c2c08bba14f2393c23bca7801a

--- drivers/usb/gadget/function/u_uvc.h
+++ drivers/usb/gadget/function/u_uvc.h
@@ -24,7 +24,6 @@ struct f_uvc_opts {
 	unsigned int					streaming_interval;
 	unsigned int					streaming_maxpacket;
 	unsigned int					streaming_maxburst;
-	unsigned int					streaming_txfifo_hint;
 
 	/*
 	 * Control descriptors array pointers for full-/high-speed and
--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -340,7 +340,6 @@ struct usb_ep {
 	unsigned		max_streams:16;
 	unsigned		mult:2;
 	unsigned		maxburst:5;
-	unsigned		txfifo_hint:5;
 	u8			address;
 	const struct usb_endpoint_descriptor	*desc;
 	const struct usb_ss_ep_comp_descriptor	*comp_desc;
