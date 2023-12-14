2fb4f24cad99f6cb03c05c7f4a85c733acf2235c
dianlujitao <dianlujitao@lineageos.org>
2023-12-14 21:53:58 +0800

    Reapply "asoc:add support for awinic algo"
    This reverts commit e600ed8b9db50dc2daabfc3cf71e5e3d5e00cc20.
    
    Change-Id: I542b965f0d3b03863367644e7f66516efa3e4960

---
+++ config/konanio.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#undef CONFIG_SND_SOC_WSA883X
+#undef CONFIG_SND_SOC_CS35L41
+#undef CONFIG_CIRRUS_PLAYBACK
+#define CONFIG_SND_SOC_AWINIC_AW882XX 1
